d3b1ab7cba9c93085311006d623e052e642c591e
Dario Lopez <dariolopezdeveloper@gmail.com>
2023-11-01 09:00:37 -0300

    Primer Commit Inicio del Proyecto

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000
+const path = require (`path`)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, './views/home.html'))
+})
+
+app.get('/contacto', (req,res)=>{
+    res.sendFile(path.join(__dirname, './views/contacto.html'))
+})
+
+app.listen(port, ()=>{
+    console.log(`El servidor esta funcionando en el puerto ${port} http://localhost:${port}`);
+})
