54183c3a1f10647d9d14fd6127c758ed17177bff
Dario Lopez <dariolopezdeveloper@gmail.com>
2023-11-14 02:48:00 -0300

    desestructuro el codigo, agrego accesos en app.js, preparo los html y css

--- app.js
+++ app.js
@@ -9,8 +9,12 @@ app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, './views/home.html'))
 })
 
-app.get('/contacto', (req,res)=>{
-    res.sendFile(path.join(__dirname, './views/contacto.html'))
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/login.html'))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/register.html'))
 })
 
 /*||||||||||| PARA EVITAR EL ERROR 404 ||||||||||||||*/
